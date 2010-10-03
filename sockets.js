25d6ad3f92c62f66965c67520b1b6f7460b4e419
Michael Bridgen <mikeb@squaremobius.net>
2010-10-03 18:18:30 +0100

    Add req and rep socket types.

--- sockets.js
+++ sockets.js
@@ -60,6 +60,49 @@ function pullSocket(client, queueName) {
     });
 }
 
+function reqSocket(client, queueName) {
+    sys.debug("req socket opened");
+    if (queueName == '') {
+        client.send("Must send address for req");
+        client.end();
+        return;
+    }
+    var replyQueue = connection.queue('', {'exclusive': true,
+                                           'autoDelete': true,
+                                           'durable': false});
+    var requestQueue = connection.queue(queueName, {'durable': true,
+                                                    'autoDelete': false});
+    replyQueue.subscribe(function(message) {
+        client.send(message.data);
+    });
+    client.on('message', function(message) {
+        connection.publish(queueName, message,
+                           {'replyTo': replyQueue.name});
+    });
+}
+
+function repSocket(client, queueName) {
+    sys.debug("rep socket opened");
+    if (queueName == '') {
+        client.send("Must send address for req");
+        client.end();
+        return;
+    }
+    var queue = connection.queue(queueName, {'durable': true,
+                                             'autoDelete': false});
+    var replyTo = '';
+    client.on('message', function (message) {
+        sys.debug("Replying to: " + replyTo);
+        connection.publish(replyTo, message);
+    });
+    queue.subscribe(function(message) {
+        console.log(sys.inspect(message, null));
+        replyTo = message._properties.replyTo;
+        sys.debug("Set reply to: " + replyTo);
+        client.send(message.data);
+    });
+}
+
 function listen(server) {
     server.on('connection', function (client) {
         function dispatch(msg) {
@@ -82,10 +125,16 @@ function listen(server) {
             case 'pull':
                 pullSocket(client, addr);
                 break;
+            case 'req':
+                reqSocket(client, addr);
+                break;
+            case 'rep':
+                repSocket(client, addr);
+                break;
             default:
                 client.send("Unknown socket type");
                 client.end();
-                sys.debug("Unknown socket type: " + msg);
+                sys.debug("Unknown socket type in: " + msg);
             }
         }
         
