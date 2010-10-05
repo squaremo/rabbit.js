8bdc4ff9572f3dc4abc8351fe6f239c03a8c1b8c
Michael Bridgen <mikeb@squaremobius.net>
2010-10-06 00:37:33 +0100

    Beef up examples

--- socketio.js
+++ socketio.js
@@ -10,11 +10,13 @@ var server = http.createServer(function (req, res) {
 	switch (path){
 	case '/':
             path = '/index.html';
-	case '/json.js':
 	case '/index.html':
+        case '/pubsub.html':
+        case '/request.html':
+        case '/reply.html':
 	    fs.readFile(__dirname + path, function(err, data){
 		if (err) return send404(res);
-		res.writeHead(200, {'Content-Type': path == 'json.js' ? 'text/javascript' : 'text/html'})
+		res.writeHead(200, {'Content-Type': 'text/html'});
 		res.write(data, 'utf8');
 		res.end();
 	    });
