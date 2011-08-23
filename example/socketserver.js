2f5f661449105a49f135705261bef295654d2637
Michael Bridgen <mikeb@squaremobius.net>
2011-08-24 00:45:23 +0100

    Rewrite socketserver example as something that ferries messages from one socket server to another via push/pull.

--- example/socketserver.js
+++ example/socketserver.js
@@ -1,10 +1,38 @@
 require.paths.unshift('.');
 
-var net = require('net');
+var net = require('net'),
+    messages = require('messages'),
+    sockets = require('sockets');
 
-var s = new net.Server();
-var ms = require('messages').listen(s);
-require('sockets').listen(ms, null, function() {
-    s.listen(9000);
-    console.log("listening on 0.0.0.0:9000");
+var server = new sockets.Server();
+
+var inSrv = net.createServer(function(connection) {
+    var incoming = server.connect();
+    incoming.send('push items');
+    connection.on('data', function(data) {
+        incoming.send(data);
+    });
+    connection.on('close', function() {
+        incoming.end();
+    });
+});
+
+var outSrv = net.createServer(function(connection) {
+    var outgoing = server.connect();
+    outgoing.on('message', function(data) {
+        connection.write(data);
+    });
+    connection.on('close', function() {
+        outgoing.end();
+    });
+    outgoing.send('pull items');
+});
+
+sockets.listen(server, {}, function() {
+    inSrv.listen(5001, function() {
+        console.log("Listening for push connections on 5001");
+    });
+    outSrv.listen(5002, function() {
+        console.log("Listening for pull connections on 5002");
+    });
 });
