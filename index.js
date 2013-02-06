a3a7c3ce3d97b331cbd8016e16a10ba930438554
Alex Damian <alex.damian@gmail.com>
2013-02-06 00:22:51 -0500

    This change allows users to specify any of the supported options in node-amqp thus giving full control over any resource they create. It also slightly modifies the client API signature to better distinguish between option types (i.e. exchange vs queue vs message) and makes interface clearer. Furthermore it allows to specify a different route key override when writing messages instead of using setsockopt() on every call. Also it is now possible to delete a permanent queue by adding two parameters (queue name and delete options) to the destroy() function.
    This code does not make any changes in the underlying code logic. It simply makes things more transparent. Because of the slight API change, it requires a minor version number change.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var sockets = require('./lib/sockets');
 
-module.exports.createContext = function(url) {
-  return new sockets.Context(url);
-}
+module.exports.createContext = function(opts) {
+  return new sockets.Context(opts);
+};
+
+module.exports.Settings = sockets.Settings;
+module.exports.Exchange = sockets.Exchange;
+module.exports.Queue = sockets.Queue;
