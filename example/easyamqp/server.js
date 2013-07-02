3a1eaecce36607cd539a7a1af7366742d9f5caf0
Michael Bridgen <mikeb@squaremobius.net>
2013-07-02 15:57:06 +0100

    easyamqp example: report stats when interrupted

--- example/easyamqp/server.js
+++ example/easyamqp/server.js
@@ -12,38 +12,41 @@ ctx.on('ready', function() {
   var sub = ctx.socket('SUB');
 
   var now = process.hrtime();
+  var i = 0;
+
+  function finish() {
+    var since = process.hrtime(now);
+    console.log("Sending and receiving %d messages took %d secs",
+                i, since[0] + since[1] * Math.pow(10, -9));
+    ctx.close();
+  }
+  process.on('SIGINT', finish);
 
   console.log('Get ready for %d messages!', NUM);
 
   sub.connect('easyamqp', function() {
     console.log("Starting consumer...");
 
-    var i = 0;
     function recv() {
       while(sub.read()) {
         i++;
         if (i % 1000 === 0) console.log("%d messages received...", i);
       }
       if (i < NUM) sub.once('readable', recv);
-      else {
-        var since = process.hrtime(now);
-        console.log("Sending and receiving %d messages took %d secs",
-                    NUM, since[0] + since[1] * Math.pow(10, -9));
-        ctx.close();
-      }
+      else finish();
     }
     recv();
 
     pub.connect('easyamqp', function() {
       console.log("Starting publisher...");
 
-      var i = 0;
+      var j = 0;
       function send() {
-        while (i < NUM && pub.write('foobar')) {
-          i++;
-          if (i % 1000 === 0) console.log('Sent %d messages', i);
+        while (j < NUM && pub.write('foobar')) {
+          j++;
+          if (j % 1000 === 0) console.log('Sent %d messages', j);
         }
-        if (i < NUM) pub.once('drain', send);
+        if (j < NUM) pub.once('drain', send);
       }
       send();
     });
@@ -52,4 +55,3 @@ ctx.on('ready', function() {
 
 });
 ctx.on('error', console.warn);
-process.on('SIGINT', ctx.close.bind(ctx));
